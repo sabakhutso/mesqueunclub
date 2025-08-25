265c759d4826cdcf16f73a372f37557fcbc070a8
sabakhutso <sxucidze11@gmail.com>
2025-08-25 17:15:54 +0400

    pirveli commit

---
+++ mesqueunclub/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine("Saba Khutsidze");
+
+Console.Write("Please Enter Your Name:");
+string Nameinput = Console.ReadLine();
+Console.WriteLine("Your Name is " + Nameinput);
